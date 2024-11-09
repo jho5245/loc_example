4b230335e134ccb8265f82681538f3936ad08060
Cucumbery <jho5245@naver.com>
2024-11-09 10:24:58 +0900

    Initial Commit

--- src/main/java/com/example/ioc_example/IocExampleApplication.java
+++ src/main/java/com/example/ioc_example/IocExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IocExampleApplication {
-
-	public static void main(String[] args) {
+public class IocExampleApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(IocExampleApplication.class, args);
 	}
-
 }
