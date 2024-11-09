9aa4f6d69b864c2df4884a430ac0091c106cd217
Cucumbery <jho5245@naver.com>
2024-11-09 10:23:48 +0900

    Initial Commit

---
+++ src/main/java/com/example/ioc_example/IocExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ioc_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ioc_example/IocExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ioc_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
